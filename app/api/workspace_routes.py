95c96096994ee1afcf02dc0e0d0bd09ba2013f81
edgar <johnnyavila2021@u.northwestern.edu>
2023-04-27 22:33:34 -0400

    add backend route handler to create a workspace

--- app/api/workspace_routes.py
+++ app/api/workspace_routes.py
@@ -6,12 +6,7 @@ from .auth_routes import validation_errors_to_error_messages
 
 workspace_routes = Blueprint('workspaces', __name__)
 
-@workspace_routes.route('/', methods=['GET'])
-@login_required
-def get_workspaces():
 
-    workspaces = Workspace.query.filter_by(owner_id=current_user.id).all()
-    return {'workspaces': [workspace.to_dict() for workspace in workspaces]}
 
 @workspace_routes.route('/<int:id>', methods=['GET'])
 @login_required
@@ -44,3 +39,36 @@ def update_workspace(id):
         return updated_workspace.to_dict()
 
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+
+@workspace_routes.route('/', methods=['POST'])
+@login_required
+def create_workspace():
+
+    form = WorkspaceForm()
+
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+    if form.validate_on_submit():
+        new_workspace = Workspace(
+            name = form.name.data,
+            description = form.description.data,
+            image_url = form.image_url.data,
+            owner_id = current_user.id
+        )
+        db.session.add(new_workspace)
+        db.session.commit()
+
+        created_workspace = Workspace.query.get(new_workspace.id)
+
+        return created_workspace.to_dict()
+
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+
+@workspace_routes.route('/', methods=['GET'])
+@login_required
+def get_workspaces():
+
+    workspaces = Workspace.query.filter_by(owner_id=current_user.id).all()
+    return {'workspaces': [workspace.to_dict() for workspace in workspaces]}
