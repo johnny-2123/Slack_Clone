270a177a4bd0589dc6ca91b2a93b212cfb72fba0
edgar <johnnyavila2021@u.northwestern.edu>
2023-04-29 05:49:18 -0400

    add user seeder data

--- app/models/user.py
+++ app/models/user.py
@@ -11,8 +11,8 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(40), nullable=False, unique=True)
-    # first_name = db.Column(db.String(35), nullable=False)
-    # last_name = db.Column(db.String(35), nullable=False)
+    first_name = db.Column(db.String(35), nullable=False)
+    last_name = db.Column(db.String(35), nullable=False)
     # image_url = db.Column(db.String)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
