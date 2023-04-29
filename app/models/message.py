5ef8a0192bc83b478de608a55fa871746707cf8a
Ethanan1 <98491284+Ethanan1@users.noreply.github.com>
2023-04-29 11:23:43 -0700

    modified conflicts in message.py

--- app/models/message.py
+++ app/models/message.py
@@ -9,27 +9,6 @@ class Message(db.Model):
         __table_args__ = {"schema": SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(255), nullable=False)
-    user_id = db.Column(db.Integer, nullable=False)
-    channel_id = db.Column(db.Integer)
-    parent_id = db.Column(db.Integer)
-    attachment_id = db.Column(db.Integer)
-    timestamp = db.Column(db.Date)
-
-    def to_dict(self):
-        return {
-            "id": self.id,
-            "content": self.content,
-            "user_id": self.user_id,
-            "channel_id": self.channel_id,
-            "parent_id": self.parent_id,
-            "attachment_id": self.attachment_id,
-            "timestamp": self.timestamp,
-        }
-        if environment == "production":
-            __table_args__ = {"schema": SCHEMA}
-
-    id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4000), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     channel_id= db.Column(db.Integer, db.ForeignKey('channels.id'))
@@ -44,3 +23,14 @@ class Message(db.Model):
     parent = db.relationship('Message', remote_side=[id], back_populates='replies')
     replies = db.relationship('Message', remote_side=[parent_id], back_populates='parent')
     # attachment = db.relationship('Attachment', back_populates='message')
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "user_id": self.user_id,
+            "channel_id": self.channel_id,
+            "parent_id": self.parent_id,
+            "attachment_id": self.attachment_id,
+            "timestamp": self.timestamp,
+        }
