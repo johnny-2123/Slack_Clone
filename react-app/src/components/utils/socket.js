5175d85abaf30b6dacc5b2ba761dc77bd58eeced
Ethanan1 <98491284+Ethanan1@users.noreply.github.com>
2023-05-11 15:39:35 -0700

    websocket implementation for DMs

--- react-app/src/components/utils/socket.js
+++ react-app/src/components/utils/socket.js
@@ -1,7 +1,7 @@
 // socket.js
-
+// creating a socket instance using the socket.io-client library and specifying the URL 'http://localhost:3000' for the WebSocket connection
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:8000');
+const socket = io('http://localhost:3000');
 
 export default socket;
