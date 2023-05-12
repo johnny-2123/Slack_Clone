7f72974ea64bcbed6b8a0b9b312e90e176a0b37e
AJ Glickauf <ajglick2000@gmail.com>
2023-05-11 20:19:51 -0500

    added schema line

--- app/models/direct_message_member.py
+++ app/models/direct_message_member.py
@@ -1,9 +1,10 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 direct_message_member = db.Table(
-    'direct_message_member',
+    'direct_message_members',
     db.Column('id', db.Integer, primary_key=True),
     db.Column('user_id', db.Integer, db.ForeignKey(add_prefix_for_prod('users.id')), nullable=False),
     db.Column('direct_message_id', db.Integer, db.ForeignKey(add_prefix_for_prod('direct_messages.id')), nullable=False),
+    schema=SCHEMA if (environment == "production") else None,
 )
 
 if environment == "production":
